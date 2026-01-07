7c3a66c362df6d5a07f5bca9f13173257939bf30
ahmedalghary1 <ahmedalghary1@gmail.com>
2026-01-07 15:25:21 +0200

    update

--- project/settings.py
+++ project/settings.py
@@ -222,6 +222,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 # CRITICAL: No default values for sensitive data - must be set in .env
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER','ahmedalghary1@gmail.com')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD','gqey nxwu ryzp rldf')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='Alwesam Talabat <info@elwsam.com>')
