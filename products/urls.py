9c7bda9082025de9eddcb2f75f2da79493298221
ahmedalghary1 <ahmedalghary1@gmail.com>
2026-02-23 13:45:42 +0200

    update

--- products/urls.py
+++ products/urls.py
@@ -8,6 +8,6 @@ app_name = 'products'
 urlpatterns = [
     path('', views.all_categories, name='all_categories'),
     path('search/', views.search_products, name='search'),
-    path('category/<slug:slug>/', views.category_products, name='category_products'),
+    path('category/<str:slug>/', views.category_products, name='category_products'),
     path('product/<str:slug>/', views.product_detail, name='product_detail'),
 ]
