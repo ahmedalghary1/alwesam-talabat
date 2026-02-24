470d877dc8f23f0f92b36c22904dd50dd7bf4a0b
ahmedalghary1 <ahmedalghary1@gmail.com>
2026-02-24 13:31:20 +0200

    add htaccess

--- project/urls.py
+++ project/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('accounts/', include('accounts.urls')),
-    path('products/', include('products.urls')),
+    path('', include('products.urls')),
     path('orders/', include('orders.urls')),
     path('cart/', include('cart.urls')),
     path('support/', include('support.urls')),  # Customer support
