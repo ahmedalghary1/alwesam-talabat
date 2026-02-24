83ab8ad40a11e65676ece1cc9d867e0b57c00641
ahmedalghary1 <ahmedalghary1@gmail.com>
2026-02-24 11:24:48 +0200

    update

--- project/settings.py
+++ project/settings.py
@@ -17,8 +17,8 @@ from datetime import timedelta
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
-
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG=True
+#DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config(
     'ALLOWED_HOSTS',
