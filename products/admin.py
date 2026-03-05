1ecec3a23ed728d8b4ba6b3c1e0473d1f0090681
ahmed algohary <ahmedalghary1@gmail.com>
2026-03-05 10:26:29 +0200

    fix

--- products/admin.py
+++ products/admin.py
@@ -1,43 +1,90 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
-from django.utils.html import format_html
-from .models import (
-    Category, Product, ProductImages, Color, Size,
-    VariantAttribute, VariantAttributeValue, ProductVariant, VariantImage
-)
 from adminsortable2.admin import SortableAdminMixin, SortableInlineAdminMixin
+from .models import (
+    Product, Category, ProductImages,
+    ProductVariant, Size,
+    VariantImage, VariantAttribute, VariantAttributeValue
+)
 
-admin.site.register(User)
 
-# ---------------- Category ----------------
-@admin.register(Category)
-class CategoryAdmin(SortableAdminMixin, admin.ModelAdmin):
-    list_display = ('name', 'order', 'image_tag')
-    search_fields = ('name',)
-    prepopulated_fields = {"slug": ("name",)}
+# -----------------------------
+# Size Admin
+# -----------------------------
+@admin.register(Size)
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ("name", "order")
+    ordering = ("order",)
+    search_fields = ("name",)
 
-# ---------------- Product Images Inline ----------------
-class ProductImagesInline(SortableInlineAdminMixin, admin.TabularInline):
+
+# -----------------------------
+# Variant Attribute System
+# -----------------------------
+class VariantAttributeValueInline(admin.TabularInline):
+    model = VariantAttributeValue
+    extra = 1
+    fields = ("value", "hex_code")
+    show_change_link = True
+
+
+@admin.register(VariantAttribute)
+class VariantAttributeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    inlines = [VariantAttributeValueInline]
+    search_fields = ("name",)
+
+
+@admin.register(VariantAttributeValue)
+class VariantAttributeValueAdmin(admin.ModelAdmin):
+    list_display = ("attribute", "value", "hex_code")
+    list_filter = ("attribute",)
+    search_fields = ("value",)
+    ordering = ("attribute", "value")
+
+
+# -----------------------------
+# Product Images Inline
+# -----------------------------
+class ProductImagesInline(admin.TabularInline):
     model = ProductImages
     extra = 1
     readonly_fields = ("image_tag",)
     fields = ("image_tag", "image", "order")
-    classes = ("collapse",)
-    
-# ---------------- Variant Image Inline ----------------
-class VariantImageInline(SortableInlineAdminMixin, admin.TabularInline):
+    show_change_link = True
+
+    def image_tag(self, obj):
+        if obj.image:
+            return f'<img src="{obj.image.url}" style="width: 60px; height:auto;" />'
+        return "-"
+    image_tag.allow_tags = True
+    image_tag.short_description = "صورة"
+
+
+# -----------------------------
+# Variant Image Inline
+# -----------------------------
+class VariantImageInline(admin.TabularInline):
     model = VariantImage
-    extra = 1
+    extra = 3
     readonly_fields = ("image_tag",)
     fields = ("image_tag", "image", "order")
-    classes = ("collapse",)
+    show_change_link = True
 
-# ---------------- ProductVariant Inline ----------------
+    def image_tag(self, obj):
+        if obj.image:
+            return f'<img src="{obj.image.url}" style="width: 60px; height:auto;" />'
+        return "-"
+    image_tag.allow_tags = True
+    image_tag.short_description = "صورة"
+
+
+# -----------------------------
+# Product Variant Inline (داخل المنتج)
+# -----------------------------
 class ProductVariantInline(SortableInlineAdminMixin, admin.TabularInline):
     model = ProductVariant
     extra = 1
-    autocomplete_fields = ("attributes", "sizes")
-    show_change_link = True
+    filter_horizontal = ("sizes", "attributes")
     fields = (
         "name",
         "code",
@@ -45,39 +92,53 @@ class ProductVariantInline(SortableInlineAdminMixin, admin.TabularInline):
         "image_tag",
         "image",
         "is_available",
-        "attributes_display",
-        "sizes_display",
+        "attributes",
+        "sizes",
         "order",
     )
-    readonly_fields = ("image_tag", "attributes_display", "sizes_display")
-    classes = ("collapse",)
+    readonly_fields = ("image_tag",)
 
-    def attributes_display(self, obj):
-        return ", ".join([str(a) for a in obj.attributes.all()])
-    attributes_display.short_description = "خصائص النمط"
+    def image_tag(self, obj):
+        if obj.image:
+            return f'<img src="{obj.image.url}" style="width: 60px; height:auto;" />'
+        return "-"
+    image_tag.allow_tags = True
+    image_tag.short_description = "صورة النمط"
 
-    def sizes_display(self, obj):
-        return ", ".join([s.name for s in obj.sizes.all()])
-    sizes_display.short_description = "المقاسات"
 
-# ---------------- Product Admin ----------------
+# -----------------------------
+# Category Admin
+# -----------------------------
+@admin.register(Category)
+class CategoryAdmin(SortableAdminMixin, admin.ModelAdmin):
+    list_display = ("name", "slug", "order")
+    prepopulated_fields = {"slug": ("name",)}
+    ordering = ("order",)
+    search_fields = ("name",)
+
+
+# -----------------------------
+# Product Admin
+# -----------------------------
 @admin.register(Product)
 class ProductAdmin(SortableAdminMixin, admin.ModelAdmin):
-    list_display = ('name', 'category', 'is_available', 'order', 'created_at')
-    search_fields = ('name', 'category__name')
-    list_filter = ('is_available', 'category')
-    inlines = [ProductImagesInline, ProductVariantInline]
-    autocomplete_fields = ('sizes',)
+    list_display = ("name", "category", "pcs_carton", "order", "is_available", "created_at")
+    list_filter = ("category", "is_available", "created_at")
+    search_fields = ("name", "description")
     prepopulated_fields = {"slug": ("name",)}
+    filter_horizontal = ("sizes",)
+    inlines = [ProductImagesInline, ProductVariantInline]
+    ordering = ("order",)
+    # تحسين عرض الصفحة الكبيرة
+    class Media:
+        css = {
+            "all": ("admin/css/custom_admin.css",)
+        }
 
-# ---------------- VariantAttributeValue Admin ----------------
-@admin.register(VariantAttributeValue)
-class VariantAttributeValueAdmin(admin.ModelAdmin):
-    list_display = ('value', 'attribute', 'hex_code')
-    search_fields = ('value', 'attribute__name')
-    autocomplete_fields = ('attribute',)
 
-# ---------------- ProductVariant Admin ----------------
+# -----------------------------
+# Product Variant Admin (صفحة مستقلة)
+# -----------------------------
 @admin.register(ProductVariant)
 class ProductVariantAdmin(SortableAdminMixin, admin.ModelAdmin):
     list_display = (
@@ -86,34 +147,21 @@ class ProductVariantAdmin(SortableAdminMixin, admin.ModelAdmin):
         "code",
         "pcs_carton",
         "is_available",
-        "color_display",
-        "sizes_display",
-        "attributes_display",
+        "order",
+        "color_name_display",
+        "sizes_list_display",
     )
-    search_fields = ("product__name", "name", "code")
     list_filter = ("is_available", "product")
-    autocomplete_fields = ("attributes", "sizes")
+    search_fields = ("product__name", "name", "code")
+    filter_horizontal = ("sizes", "attributes")
     inlines = [VariantImageInline]
+    ordering = ("order",)
 
-    class Media:
-        css = {
-            'all': ('admin/css/custom_admin.css',)  # الملف الذي سننشئه
-        }
-
-    def color_display(self, obj):
+    def color_name_display(self, obj):
         color = obj.color
-        if color and color.hex_code:
-            return format_html(
-                '<span style="background:{};color:#fff;padding:2px 6px;border-radius:3px;font-size:12px;">{}</span>',
-                color.hex_code, color.value
-            )
         return color.value if color else "-"
-    color_display.short_description = "اللون"
+    color_name_display.short_description = "اللون"
 
-    def sizes_display(self, obj):
-        return ", ".join([s.name for s in obj.sizes.all()])
-    sizes_display.short_description = "المقاسات"
-
-    def attributes_display(self, obj):
-        return ", ".join([str(a) for a in obj.attributes.all()])
-    attributes_display.short_description = "خصائص النمط"
+    def sizes_list_display(self, obj):
+        return ", ".join([size.name for size in obj.sizes.all()])
+    sizes_list_display.short_description = "المقاسات"
