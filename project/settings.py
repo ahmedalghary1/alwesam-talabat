ce5e47701df2ed268e7715487864ede877cb1f6a
ahmed algohary <ahmedalghary1@gmail.com>
2026-03-08 11:20:38 +0200

    update email settings

--- project/settings.py
+++ project/settings.py
@@ -198,18 +198,21 @@ COMPRESS_ENABLED = False
 # ==================================================
 # Email
 # ==================================================
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST = "mail.elwsamstore.com"
+EMAIL_PORT = 465
+
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+
 DEFAULT_FROM_EMAIL = config(
-    'DEFAULT_FROM_EMAIL',
-    default='Alwesam Talabat <info@elwsam.com>'
+    "DEFAULT_FROM_EMAIL",
+    default="Alwesam Talabat <support@elwsamstore.com>"
 )
-
 
 
 
